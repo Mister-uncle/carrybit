14c7a3162190cbfa2a6ca18e59ebbafee3f8fe13
chenansong <1327030921@qq.com>
2020-09-18 01:36:53 +0800

    完善了现有的bug

--- js/register.js
+++ js/register.js
@@ -1,33 +1,3 @@
-$('.input_user input').on("click",function () {
-    if(this.value =='请输入邮箱'){
-        this.value = '';
-    }
-});
-$('.input_user_one input').on("blur",function () {
-    if(this.value == ''){
-        this.value = '请输入邮箱';
-    }
-});
-$('.input_password input').on("click",function () {
-    if(this.value == '请输入密码'){
-        this.value = '';
-    }
-});
-$('.input_password input').on("blur",function () {
-    if(this.value == ''){
-        this.value = '请输入密码';
-    }
-});
-$('.input_password_two input').on("click",function () {
-    if(this.value == '请再次输入密码'){
-        this.value = '';
-    }
-});
-$('.input_password_two input').on("blur",function () {
-    if(this.value == ''){
-        this.value = '请再次输入密码';
-    }
-});
 
 var inputChecked = $('.re_main_regist_bot input');
 var re_btn = $('.re_regist');
