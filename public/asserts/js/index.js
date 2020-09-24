729e94a02dd143112460eb6f7ed4463e790f2614
chenansong <1327030921@qq.com>
2020-09-25 02:29:11 +0800

    后台商城jq页面切换

--- public/asserts/js/index.js
+++ public/asserts/js/index.js
@@ -1,6 +1,71 @@
-console.log((1))
-$("#goodsManage").on("click",function () {
-    alert("yes");
-    console.log(2);
+$(function () {
+    let lis = $(".content_sideShop li");
+    let li = $(".togleDesk");
+
+    lis.on("click",function () {
+        $(this).addClass('current');
+        $(this).siblings().removeClass('current');
+        var index = $(this).index();
+        li.eq(index).removeClass("deskActive");
+        li.eq(index).siblings().addClass("deskActive");
+    })
+
+    let topli = $(".topCenter li");
+    let side = $(".content_side ul");
+    topli.on("click",function () {
+        $(this).addClass("current");
+        $(this).siblings().removeClass("current");
+        var index = $(this).index();
+        console.log(typeof (index))
+        side.eq(index).show();
+        side.eq(index).siblings().hide();
+    })
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
