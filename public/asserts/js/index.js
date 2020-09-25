432f4a58cb0d00e9296e7e855d68e724c3a42881
chenansong <1327030921@qq.com>
2020-09-25 12:54:10 +0800

    后台界面1.0

--- public/asserts/js/index.js
+++ public/asserts/js/index.js
@@ -1,7 +1,7 @@
 $(function () {
     let lis = $(".content_sideShop li");
     let li = $(".togleDesk");
-
+    //左侧栏点击后切换li状态和内容区的显示li
     lis.on("click",function () {
         $(this).addClass('current');
         $(this).siblings().removeClass('current');
@@ -9,16 +9,27 @@ $(function () {
         li.eq(index).removeClass("deskActive");
         li.eq(index).siblings().addClass("deskActive");
     })
-
+    //顶部导航切换效果，以及左侧显示内容
     let topli = $(".topCenter li");
     let side = $(".content_side ul");
     topli.on("click",function () {
         $(this).addClass("current");
         $(this).siblings().removeClass("current");
         var index = $(this).index();
-        console.log(typeof (index))
         side.eq(index).show();
         side.eq(index).siblings().hide();
+    })
+
+    let conli = $(".content_sideSystem li");
+    let condiv = $(".content_side");
+
+    condiv.on("click",".content_sideSystem li",function () {
+        $(this).addClass("current");
+        $(this).siblings().removeClass("current");
+        var index = $(this).index()+3;
+        console.log(index)
+        li.eq(index).removeClass("deskActive");
+        li.eq(index).siblings().addClass("deskActive");
     })
 
 })
